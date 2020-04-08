c27e2a1de0dc9040616fe493980a076187737008
Guilherme Fünkler Borelli <borelli.guilherme@gmail.com>
2020-04-08 13:01:18 -0300

    Update index.js to redux
    Implemented redux to index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,11 +5,23 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import reducers from './reducers/reducers'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux';
+
+import thunk from 'redux-thunk'
+
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const store = createStore(reducers, composeWithDevTools(applyMiddleware(thunk)));
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
