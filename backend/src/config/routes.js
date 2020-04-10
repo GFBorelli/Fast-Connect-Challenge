b75e677e1e6a7bc2cf1edbe8debb3f9f9c5cf8ce
Guilherme Fünkler Borelli <borelli.guilherme@gmail.com>
2020-04-10 15:56:50 -0300

    Added JWT on backednd

--- backend/src/config/routes.js
+++ backend/src/config/routes.js
@@ -1,10 +1,24 @@
 const express = require('express')
+const auth = require('./auth')
 
 module.exports = function (server) {
+    /*
+    * Rotas protegidas por Token JWT
+    */
+    const protectedApi = express.Router()
+    server.use('/api', protectedApi)
+    protectedApi.use(auth)
 
-    const router = express.Router()
-    server.use('/api', router)
+    const saleService = require('../api/sales/saleService')
+    saleService.register(protectedApi, '/sales')
+    /*
+    * Rotas abertas
+    */
+    const openApi = express.Router()
+    server.use('/oapi', openApi)
 
-    const saleService = require('../api/saleService')
-    saleService.register(router, '/sales')
+    const AuthService = require('../api/user/authService')
+    openApi.post('/login', AuthService.login)
+    openApi.post('/signup', AuthService.signup)
+    openApi.post('/validateToken', AuthService.validateToken)
 }
